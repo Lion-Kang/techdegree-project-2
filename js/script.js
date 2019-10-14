c9a99058b3ff2bac7780918ce7f8fd471e128537
Lion-Kang <53907943+Lion-Kang@users.noreply.github.com>
2019-10-13 20:46:02 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,16 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Global Variables
 listItems = document.querySelectorAll('li') ;
 itemsPerPage = 10;
 
+// ***FUNCTION showPage*** - This function shows and hides sets of 10 students in the list
 const showPage = (list, page) => {
    startIndex = (page * itemsPerPage) - itemsPerPage;
    endIndex = (page * itemsPerPage);
 
+// Loop through the list items to display 10 on each page
    for (i = 0; i < listItems.length; i += 1){
       if ((i < endIndex) && (i >= startIndex)) {
          listItems[i].style.display = 'block';
@@ -22,18 +25,31 @@ const showPage = (list, page) => {
    }
 
 }
-showPage(listItems, 1);
+showPage(listItems, 2);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-
+// ***FUNCTION appendPageLinks*** - This function is supposed to create buttons for each page (NOT FINISHED) 
 const appendPageLinks = (list) => {
+   // Ceil the amount of list items to find the amount of pages needed   
    totalPages = Math.ceil(listItems.length/itemsPerPage);
-   console.log(totalPages);
+   //console.log(totalPages);
+   
+   // Create button 
+   nextButton = document.createElement('nextButton');
+   
+   // ??? Set page to button ???
+   nextButton.innerHTML = page;
+   const button = document.getElementsByTagName('button')[0];
+   // Set listener on button 
+   nextButton.addEventListener ('click', function() {
+      showPage(listItems, page)
+   })
 
+   // Create HTML elements for pagination links
    paginationDiv = document.createElement('div');
    paginationDiv.className = ('pagination');
    page = document.querySelector('.page');
@@ -41,13 +57,11 @@ const appendPageLinks = (list) => {
    ul = document.createElement('ul');
    paginationDiv.appendChild(ul);
 
+   // add 'li' and 'a' to all pagination elements
    for (i = 0; i < page.length; i += 1) {
       let li = document.createElement('li');
       let a = document.createElement('a');
-      a.href = '#';
+      a.href = '#'; 
    }
 }
 appendPageLinks(listItems);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
