ed0295c5046d1dc5ce7ba33288b9d5862817cabb
Lion-Kang <53907943+Lion-Kang@users.noreply.github.com>
2019-10-24 17:10:49 -0700

    Finished Pagination
    dynamically added html buttons for each page

--- js/script.js
+++ js/script.js
@@ -5,16 +5,13 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-// Global Variables
 listItems = document.querySelectorAll('li') ;
 itemsPerPage = 10;
 
-// ***FUNCTION showPage*** - This function shows and hides sets of 10 students in the list
 const showPage = (list, page) => {
-   startIndex = (page * itemsPerPage) - itemsPerPage;
-   endIndex = (page * itemsPerPage);
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = (page * itemsPerPage);
 
-// Loop through the list items to display 10 on each page
    for (i = 0; i < listItems.length; i += 1){
       if ((i < endIndex) && (i >= startIndex)) {
          listItems[i].style.display = 'block';
@@ -25,24 +22,49 @@ const showPage = (list, page) => {
    }
 
 }
+showPage(listItems, 1);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-// ***FUNCTION appendPageLinks*** - This function is supposed to create buttons for each page (NOT FINISHED) 
+
 const appendPageLinks = (list) => {
-   // total amount of students in the list
-   let totalStudents = listItems.length
-   // Ceil the amount of list items to find the amount of pages needed   
    let totalPages = Math.ceil(listItems.length/itemsPerPage);
-   // Select page number to pass through showPage function to select page number
-   let selectedButton = 1;
+   console.log("Total amount of pages: ", totalPages);
+   let totalStudents = listItems.length;
+   let selected_button = 1;
+   let ul = document.querySelector('.pagination_buttons');
+   console.log("This is the ul", ul);
 
-   showPage(list, selectedButton)
+   showPage(listItems, selected_button);
 
-   // create button for pages
-
+   for (i = 0; i < totalPages; i += 1) {
+      console.log("building page", i);
+      // dynamically add pagination links
+      let li_button = document.createElement('li');
+      let a = document.createElement('a');
+      a.href = '#';
+      li_button.appendChild(a);
+      ul.appendChild(li_button);
+      // set page number starting at 1 instead of zero
+      if(i == 0) {
+         a.className = 'active';
+      }
+      a.textContent = i + 1;
+   }
+   // add click handler to each button using
+   ul.addEventListener('click', (e) => {
+      let previous_button = document.querySelector('.active');
+      previous_button.className = '';
+      selected_button = e.target.textContent;
+      e.target.className = 'active';
+      showPage(listItems, selected_button);
+   }
+   )
 }
 appendPageLinks(listItems);
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
